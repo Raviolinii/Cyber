23102c43a8e61296b4e915321ddcd0cfc236adb2
Maksiolo20 <maksiolo2000@interia.pl>
2022-10-14 22:55:24 +0200

    Added empty AdminPanel controller and view

---
+++ Cyber/Controllers/AdminPanelController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cyber.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
