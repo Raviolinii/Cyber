03dc3586e5ef40014a143e25b8d451aa1eba96f9
Maksiolo20 <maksiolo2000@interia.pl>
2022-11-18 22:29:12 +0100

    Re-added google reCapcha

---
+++ Cyber/Models/SiteSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Cyber.Models
+{
+    public static class SiteSettings
+    {
+        public const string GoogleRecaptchaSecretKey = "";
+        public const string GoogleRecaptchaSiteKey = "";
+    }
+}
